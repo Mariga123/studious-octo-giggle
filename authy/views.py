a2dcc12405c0b36be73c1a90ef365b443a638607
Mariga123 <johnmariga8@gmail.com>
2021-01-19 10:35:17 +0300

    create follow and unfollow buttons,profile page

--- authy/views.py
+++ authy/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from authy.forms import SignupForm, ChangePasswordForm, EditProfileForm
 from django.contrib.auth.models import User
-from post.models import Post,Follow
+from post.models import Post, Follow, Stream
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import update_session_auth_hash
 
 from authy.models import Profile
 from django.template import loader
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 
 from django.core.paginator import Paginator
-from django.urls import resolve
+from django.urls import resolve, reverse
+from django.db import transaction
 
 # Create your views here.
 def UserProfile(request, username):
@@ -29,6 +30,9 @@ def UserProfile(request, username):
 	following_count = Follow.objects.filter(follower=user).count()
 	followers_count = Follow.objects.filter(following=user).count()
 
+	# check follow status
+	follow_status = Follow.objects.filter(following=user, follower=request.user).exists()
+
 	#Pagination
 	paginator = Paginator(posts, 8)
 	page_number = request.GET.get('page')
@@ -43,6 +47,7 @@ def UserProfile(request, username):
 		'posts_count':posts_count,
 		'followers_count': followers_count,
 		'following_count':following_count,
+		'follow_status':follow_status,
 	}
 
 	return HttpResponse(template.render(context, request))
@@ -115,3 +120,27 @@ def EditProfile(request):
 	}
 
 	return render(request, 'edit_profile.html', context)
+
+@login_required
+def follow(request, username, option):
+	user = request.user
+	following = get_object_or_404(User, username=username)
+
+	try:
+		f, created = Follow.objects.get_or_create(follower=user, following=following)
+
+		if int(option) == 0:
+			f.delete()
+			Stream.objects.filter(following=following, user=user).all().delete()
+		else: 
+			posts = Post.objects.all().filter(user=following)[:10]
+
+			with transaction.atomic():
+				for post in posts:
+					stream = Stream(post=post, user=user, date=post.posted, following=following)
+					stream.save()
+
+		return HttpResponseRedirect(reverse('profile', args=[username]))
+
+	except User.DoesNotExist:
+		return HttpResponseRedirect(reverse('profile', args=[username]))
