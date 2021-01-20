7b43ab4c2065317042a2ca216e1148bbe4b48b17
Mariga123 <johnmariga8@gmail.com>
2021-01-20 04:46:22 +0300

    install decouple

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -81,11 +81,16 @@ WSGI_APPLICATION = 'instagram_clone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
